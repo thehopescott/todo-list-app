f5f84e2947bfcebb8c32b41798a032394f838c56
Hope Scott <hopscott@protonmail.com>
2022-04-16 07:09:34 +0200

    Login Process; Authentication. (Complete)

--- dist/js/app.js
+++ dist/js/app.js
@@ -22,14 +22,18 @@ formSwitches.forEach(
 
 // AUTHENTICATION PROCESS
 //User Registration
+const authWindow = document.querySelector(".auth-container");
 const regFormBtn = document.querySelector(".reg-form button");
-const authFormAlert = document.querySelector(".msg-alert");
+const loginFormBtn = document.querySelector(".login-form button");
+const regFormAlert = document.querySelector(".reg-form .msg-alert");
+const loginFormAlert = document.querySelector(".login-form .msg-alert");
 const fieldEmptyMsg = "Please fill all fields before submitting.";
 const regFailedMsg = "Email already exists. Try again.";
+const loginFailedMsg = "User details are incorrect. Try again";
 const regSuccessMsg = "Registration Successful! You can now proceed to login.";
 let allUsers = [];
-//Retreiving available users from localStorage
 
+//Retreiving available users from localStorage
 if (localStorage.getItem("users") == null) {
     localStorage.setItem("users", JSON.stringify(allUsers));
 } else {
@@ -46,6 +50,14 @@ class NewUser {
     }
 }
 
+//Creating an array of available user emails for login and registration validation
+const allUserNames = [];
+const allUserEmails = [];
+allUsers.forEach((user) => {
+    allUserNames.push(user.fullName);
+    allUserEmails.push(user.userEmail);
+});
+
 function userCreation() {
     //Creating userHandle from the submitted email
     let userHandle = document.querySelector("#reg-email").value.split("@")[0];
@@ -60,47 +72,84 @@ function userCreation() {
     //Appending the new user to the list of available users and saving to locaStorage
     allUsers.push(newUser);
     localStorage.setItem("users", JSON.stringify(allUsers));
+    document.querySelector(".auth-loader").style.width = "100%";
+    location.reload();
 }
 
-function displayStatusMessage(color, msg) {
-    authFormAlert.textContent = msg;
-    authFormAlert.style.color = color;
-    authFormAlert.style.display = "block";
-    authFormAlert.style.opacity = 1;
+function displayRegistrationStatusMessage(color, msg) {
+    regFormAlert.textContent = msg;
+    regFormAlert.style.color = color;
+    regFormAlert.style.display = "block";
+    regFormAlert.style.opacity = 1;
+}
+function displayLoginStatusMessage(color, msg) {
+    loginFormAlert.textContent = msg;
+    loginFormAlert.style.color = color;
+    loginFormAlert.style.display = "block";
+    loginFormAlert.style.opacity = 1;
 }
 
 regFormBtn.addEventListener("click", (e) => {
     e.preventDefault();
-
-    //User information validation/checking for resubmissions/double registrations
-    const allUserEmails = [];
-    allUsers.forEach((user) => {
-        allUserEmails.push(user.userEmail);
-    });
     if (
         document.querySelector("#reg-fullname").value === "" ||
         document.querySelector("#reg-email").value === "" ||
         document.querySelector("#job").value === ""
     ) {
-        displayStatusMessage("red", fieldEmptyMsg);
+        displayRegistrationStatusMessage("red", fieldEmptyMsg);
         setTimeout(() => {
-            authFormAlert.style.display = "none";
+            regFormAlert.style.display = "none";
         }, 3000);
     } else if (
         allUserEmails.includes(document.querySelector("#reg-email").value)
     ) {
-        displayStatusMessage("red", regFailedMsg);
+        displayRegistrationStatusMessage("red", regFailedMsg);
         setTimeout(() => {
-            authFormAlert.style.display = "none";
+            regFormAlert.style.display = "none";
         }, 3000);
     } else {
         userCreation();
         document.querySelectorAll(".reg-form input").forEach((inputField) => {
             inputField.value = "";
         });
-        displayStatusMessage("green", regSuccessMsg);
+        displayRegistrationStatusMessage("green", regSuccessMsg);
         setTimeout(() => {
-            authFormAlert.style.display = "none";
+            regFormAlert.style.display = "none";
+        }, 3000);
+    }
+});
+
+//User Login
+loginFormBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (
+        document.querySelector("#login-fullname").value === "" ||
+        document.querySelector("#login-email").value === ""
+    ) {
+        displayLoginStatusMessage("red", fieldEmptyMsg);
+        setTimeout(() => {
+            loginFormAlert.style.display = "none";
+        }, 3000);
+    } else if (
+        allUserEmails.includes(document.querySelector("#login-email").value) &&
+        allUserNames.includes(
+            document.querySelector("#login-fullname").value
+        ) &&
+        allUserEmails.indexOf(
+            document.querySelector("#login-email").value ===
+                allUserNames.indexOf(
+                    document.querySelector("#login-fullname").value
+                )
+        )
+    ) {
+        document.querySelector(".auth-loader").style.width = "100%";
+        setTimeout(() => {
+            authWindow.style.display = "none";
+        }, 3000);
+    } else {
+        displayLoginStatusMessage("red", loginFailedMsg);
+        setTimeout(() => {
+            loginFormAlert.style.display = "none";
         }, 3000);
     }
 });
